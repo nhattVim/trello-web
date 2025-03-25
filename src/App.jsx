771e608a8c88c19db90e3bad36b770959acf4a5f
nhattVim <nhattruong13112000@gmail.com>
2025-03-25 07:59:17 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
+
 
 function App() {
   return (
     <>
+      <Typography variant="body2" color="text.secondary">Test typography</Typography>
       <div>nhattVim</div>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="contained" color='success'>Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
